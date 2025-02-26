748ef940c3be74b053b26cba6ae119bcba8191bb
Tarun Prajapati <tarun.prajapati912@gmail.com>
2025-02-27 00:37:17 +0530

    stack questions added

--- Stack/Questions.java
+++ Stack/Questions.java
@@ -35,6 +35,140 @@ public class Questions {
         pushAtBottom(s, top);
     }
 
+    public static int[] stockSpan(int stocks[]) {
+        Stack<Integer> s = new Stack<>();
+        int span[] = new int[stocks.length];
+        span[0] = 1;
+        s.push(0); // push index
+
+        for (int i = 1; i < stocks.length; i++) {
+            int currPrice = stocks[i];
+
+            while (!s.isEmpty() && currPrice > stocks[s.peek()]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                span[i] = i + 1;
+            } else {
+                span[i] = i - s.peek(); // i - prevHigh
+            }
+        }
+        return span;
+    }
+
+    public static int[] nextGreater(int arr[]) {
+        int n = arr.length;
+        int result[] = new int[n];
+
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+
+            if (s.isEmpty()) {
+                result[i] = -1; // no nextGreater Present
+            } else {
+                result[i] = arr[s.peek()];
+            }
+            s.push(i);
+        }
+        return result;
+    }
+
+    public static boolean isValid(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                s.push(ch);
+            } else {
+                if (s.isEmpty()) {
+                    return false;
+                }
+                if ((s.peek() == '(' && ch == ')')
+                        || (s.peek() == '{' && ch == '}')
+                        || (s.peek() == '[' && ch == ']')) {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return s.isEmpty();
+    }
+
+    public static boolean isDuplicate(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // closing
+            if (ch == ')') {
+                int count = 0;
+                while (s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+                if (count < 1) {
+                    return true; // duplicate exist
+                } else {
+                    s.pop(); // opening pair
+                }
+            } else { // opening
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+
+    public static int maxArea(int arr[]) {
+        int maxArea = 0, n = arr.length;
+        int nsr[] = new int[n];
+        int nsl[] = new int[n];
+
+        // next smaller right
+        Stack<Integer> s = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsr[i] = n;
+            } else {
+                nsr[i] = s.peek(); // index
+            }
+            s.push(i);
+        }
+
+        // next smaller left
+        s = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // current area
+        for (int i = 0; i < n; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1; // j-i-1
+            int currArea = height * width;
+            maxArea = Math.max(maxArea, currArea);
+        }
+        return maxArea;
+    }
+
     public static void printStack(Stack<Integer> s) {
         while (!s.isEmpty()) {
             System.out.println(s.pop());
@@ -42,12 +176,20 @@ public class Questions {
     }
 
     public static void main(String args[]) {
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
 
-        reverseStack(s);
-        printStack(s);
+        // reverseStack(s);
+        // printStack(s);
+        // int stocks[] = { 6, 8, 0, 1, 3 };
+        // int span[] = nextGreater(stocks);
+
+        // for (int n : span) {
+        // System.out.println(n);
+        // }
+        int arr[] = { 2, 1, 5, 6, 2, 3 };
+        System.out.println(maxArea(arr));
     }
 }
